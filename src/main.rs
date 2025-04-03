3b83d582886db21df4d54a81c1f56334f89ac5dd
shricubed <ishrikar@live.com>
2025-04-02 20:21:26 -0400

    client using tokio rustls

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use tokio::net::{TcpListener, TcpStream};
-use tokio::io::{self};
+use rustls::pki_types::pem::PemObject;
+use rustls::pki_types::{CertificateDer, ServerName};
 use structopt::StructOpt;
+use openssl::ssl::{SslMethod, SslAcceptor, SslContext, Ssl, SslStream, SSL_VERIFY_NONE};use openssl::x509::X509FileType;
+use tokio::io::{copy, split, stdin as tokio_stdin, stdout as tokio_stdout, AsyncReadExt, AsyncWriteExt};
+use tokio_rustls::{rustls, TlsConnector, TlsAcceptor};
+
+use std::io;
+use std::net::ToSocketAddrs;
+use std::sync::Arc;
 
 #[derive(StructOpt)]
 struct ProxyConfig {
@@ -25,20 +33,43 @@ async fn handle_server(local: String, remote: String) -> io::Result<()> {
     }
 }
 
-async fn handle_client(mut client: TcpStream, remote: String) {
+async fn handle_client(mut client: TcpStream, remote: String, message: String, cafile: PathBuf) {
+    
+    let mut root_store = rustls::RootCertStore::empty();
+    for cert in CertificateDer::pem_file_iter(cafile)? {
+        root_store.add(cert?)?;
+    }
 
+    let config = rustls::ClientConfig::builder()
+        .with_root_certificates(root_store)
+        .with_safe_defaults()
+        .with_no_client_auth();
+
+    let connector = TlsConnector::from(Arc::new(config));
     match TcpStream::connect(remote).await {
         Ok(mut outbound) => {
-            let (mut ri, mut wi) = client.split();
-            let (mut ro, mut wo) = outbound.split();
-
-            let client_to_remote = io::copy(&mut ri, &mut wo);
-            let remote_to_client = io::copy(&mut ro, &mut wi);
+            let domain = ServerName::try_from(remote)?.to_owned();
+            let mut tls = connector.connect(domain, outbound).await.unwrap();
+            outbound.write_all(message.as_bytes()).await.unwrap();
+            let (mut client_reader, mut client_writer) = split(outbound);
+            let (mut stdin, mut stdout) = (tokio_stdin(), tokio_stdout());
 
             tokio::select! {
-                _ = client_to_remote => {},
-                _ = remote_to_client => {},
+                res = copy(&mut client_reader, &mut stdout) => {
+                    if let Err(e) = res {
+                        eprintln!("Error copying from client to stdout: {}", e);
+                    }
+                },
+
+                res = copy(&mut stdin, &mut client_writer) => {
+                    if let Err(e) = res {
+                        eprintln!("Error copying from stdin to client: {}", e);
+                    }
+                }
             }
+
+            Ok(())
+
         }
 
         Err(e) => {
